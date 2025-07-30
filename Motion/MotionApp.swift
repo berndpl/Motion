1bc5d8aeed61d20c21015e8fdd0fc0f6f467893d
Bernd Plontsch <bernd.plontsch@gmail.com>
2025-07-30 16:03:25 +0900

    Initial Commit

---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Bernd Plontsch on 30.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
